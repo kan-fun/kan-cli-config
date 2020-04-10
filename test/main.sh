1f710e62aede9110c5112ad992384af66d8f6d83
Your Name <you@example.com>
2020-04-10 12:34:08 +0800

    Fix bug

--- test/main.sh
+++ test/main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-mv ./kan-cli-config ./kan-config
 
-./kan-config init --access-key 123 --secret-key 456
+./kan-cli-config init --access-key 123 --secret-key 456
 diff ~/.kanrc.yml ./test/init.yml
