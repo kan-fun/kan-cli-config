e37e760f3b5ab7e286680831a3c99f0c2edbbcb5
Your Name <you@example.com>
2020-04-10 13:22:58 +0800

    Add version

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-# wget http://gosspublic.alicdn.com/ossutil/1.6.10/ossutil64
-# chmod 755 ossutil64
-# ./ossutil64 config -e oss-cn-beijing.aliyuncs.com -i $ACCESSKEY -k $SECRETKEY
-# ./ossutil64 rm -rf oss://kan-bin/linux --include "kan-config_*"
-# ./ossutil64 cp -f ./kan-cli-config oss://kan-bin/linux/kan-config_0.0.1
+wget http://gosspublic.alicdn.com/ossutil/1.6.10/ossutil64
+chmod 755 ossutil64
+./ossutil64 config -e oss-cn-beijing.aliyuncs.com -i $ACCESSKEY -k $SECRETKEY
+./ossutil64 rm -rf oss://kan-bin/linux --include "kan-config_*"
 
-echo 123
-echo 456
-echo $GITHUB_REF
+version=`echo $GITHUB_REF | cut -d "/" -f 3`
+
+./ossutil64 cp -f ./kan-cli-config "oss://kan-bin/linux/kan-config_$version"
